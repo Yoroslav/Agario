4f17a5b42abdc87a3003adba03687eb1ccc7f153
Yoroslav <140841390+Yoroslav@users.noreply.github.com>
2025-01-25 11:28:05 +0200

    Added
    1. Separate the controller
    2. Start making a system of inputs designed to scale the number of those inputs. F from the lesson is an example of one of them

---
+++ Agario/KeyboardInputHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace Engine
+{
+    public class InputController
+    {
+        private readonly Dictionary<string, Func<bool>> _inputActions;
+
+        public InputController()
+        {
+            _inputActions = new Dictionary<string, Func<bool>>();
+        }
+
+        public void BindAction(string actionName, Func<bool> inputCondition)
+        {
+            _inputActions[actionName] = inputCondition;
+        }
+
+        public bool IsActionTriggered(string actionName)
+        {
+            return _inputActions.ContainsKey(actionName) && _inputActions[actionName]();
+        }
+    }
+}
